d249b425dda89e3f8ae15e1346b9d2140aae199d
JulioCesar-A <juliocesar02e@gmail.com>
2025-08-31 04:50:41 -0300

    Refactor Club Repository: Atualizar operações com ffle system para usar promises

--- src/repositories/club-repository.ts
+++ src/repositories/club-repository.ts
@@ -1,6 +1,6 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
-import { ClubResponse } from '../models/club/club-models';
+import { ClubResponse, Club, ClubUpdate } from '../models/club/club-models';
 
 
 const clubsFilePath = path.join(__dirname, '../../data/clubs.json');
@@ -8,10 +8,9 @@ const clubsFilePath = path.join(__dirname, '../../data/clubs.json');
 export const getAllClubs = async() : Promise<ClubResponse[]> => {
     let clubsData: ClubResponse[] = [];
     try {
-        if (fs.existsSync(clubsFilePath)) {
-            const fileContent = fs.readFileSync(clubsFilePath, 'utf-8');
-            clubsData = JSON.parse(fileContent);
-        }
+        const fileContent = await fs.readFile(clubsFilePath, 'utf-8');
+        clubsData = JSON.parse(fileContent);
+        
     } catch (error) {
         console.error('Error reading clubs data:', error);
     }
@@ -28,8 +27,36 @@ export const getClubById = async (id : number) : Promise<ClubResponse | undefine
 
 export const getClubByName = async (name : string) : Promise<ClubResponse | undefined> => {
     let clubsData: ClubResponse[] = await getAllClubs();
-
+    
     const foundClub : ClubResponse | undefined = clubsData.find((club) => club.name.includes(name));
 
     return foundClub;
+}
+
+export const insertClub = async(club: Club) : Promise<ClubResponse> =>{
+    let clubsData: ClubResponse[] = await getAllClubs();
+
+    const existingIndex = clubsData.findIndex((c) => c.name === club.name && c.location === club.location);
+    if (existingIndex !== -1) {
+        throw new Error('Club already exists');
+    }
+
+    const maxId = clubsData.reduce((max, club) => club.id > max ? club.id : max, 0);
+    const newClub: ClubResponse = {
+        ...club,
+        id: maxId + 1
+    }
+
+    clubsData.push(newClub);
+
+    try {
+        await fs.writeFile(clubsFilePath, JSON.stringify(clubsData, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Failed to write club data to file', error);
+        throw new Error('Could not save club to database');
+
+    }
+    console.table(clubsData);
+
+    return clubsData[clubsData.length-1];
 }
