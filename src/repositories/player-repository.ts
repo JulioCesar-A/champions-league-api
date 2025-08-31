018607b2a6469cd37fabafa8dfe4f6d00a798184
JulioCesar-A <juliocesar02e@gmail.com>
2025-08-30 23:50:15 -0300

    Player Repository: Função para listar todos os jogadores - getAllPlayers()

---
+++ src/repositories/player-repository.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { PlayerResponse } from '../models/player/player-models';
+
+const playersFilePath = path.join(__dirname, '../../data/players.json');
+
+export const getAllPlayers = async() : Promise<PlayerResponse[]> => {
+    let playersData : PlayerResponse[] = [];
+
+    try {
+        if (fs.existsSync(playersFilePath)) {
+            const fileContent = fs.readFileSync(playersFilePath, 'utf-8');
+            playersData = JSON.parse(fileContent);
+        }
+    } catch (error) {
+        console.error('Error reading clubs data:', error);
+    }
+
+    return playersData;
+}
+
