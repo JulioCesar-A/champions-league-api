48e2ba74db37383fb9e38bc433acb0c88979759f
JulioCesar-A <juliocesar02e@gmail.com>
2025-08-30 23:27:22 -0300

    Club Repository: Função para listar todos os clubes - getAllClubs()

--- src/repositories/club-repository.ts
+++ src/repositories/club-repository.ts
@@ -1,9 +1,19 @@
 import fs from 'fs';
 import path from 'path';
+import { ClubTransferModel } from '../models/club/club-transfer-models';
 
 
 const clubsFilePath = path.join(__dirname, '../../data/clubs.json');
-const clubsData = fs.existsSync(clubsFilePath) ? JSON.parse(fs.readFileSync(clubsFilePath, 'utf-8')) : "l";
 
-
-console.table(clubsData);
+async function getAllClubs() : Promise<ClubTransferModel[]> {
+    let clubsData: ClubTransferModel[] = [];
+    try {
+        if (fs.existsSync(clubsFilePath)) {
+            const fileContent = fs.readFileSync(clubsFilePath, 'utf-8');
+            clubsData = JSON.parse(fileContent);
+        }
+    } catch (error) {
+        console.error('Error reading clubs data:', error);
+    }
+    return clubsData;
+}
