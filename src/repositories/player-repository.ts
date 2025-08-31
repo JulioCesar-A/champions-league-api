505e3af4574a55dcb44a656d2354ff5a492a1849
JulioCesar-A <juliocesar02e@gmail.com>
2025-08-31 02:34:43 -0300

    Player Repository:  Função para calcular média de estatísticas - estimateOverallStat()

--- src/repositories/player-repository.ts
+++ src/repositories/player-repository.ts
@@ -1,8 +1,19 @@
 import fs from 'fs';
 import path from 'path';
-import { PlayerResponse } from '../models/player/player-models';
+import { Player, PlayerResponse, Statistics } from '../models/player/player-models';
 
 const playersFilePath = path.join(__dirname, '../../data/players.json');
+
+const estimateOverallStat = async(stats : Statistics) : Promise<number> => {
+    let statsSum: number = 0;
+
+    for (const stat of Object.values(stats)) {
+        statsSum += stat;
+    }
+
+    const overall: number = statsSum/Object.keys(stats).length;
+    return parseFloat(overall.toFixed(1));
+}
 
 export const getAllPlayers = async() : Promise<PlayerResponse[]> => {
     let playersData : PlayerResponse[] = [];
@@ -29,8 +40,19 @@ export const getPlayerById = async(id: number) : Promise<PlayerResponse | undefi
 
 export const getPlayerByName = async(name: string) : Promise<PlayerResponse | undefined> => {
     let playersData: PlayerResponse[] = await getAllPlayers();
-
-    const foundPlayer: PlayerResponse | undefined = playersData.find((player) => player.name.includes(name.));
-
+    
+    const foundPlayer: PlayerResponse | undefined = playersData.find((player) => player.name.includes(name));
+    
     return foundPlayer;
 }
+
+// export const insertPlayer = async(player : Player, clubId?: number) : Promise<void> => {
+//     let playersData: PlayerResponse[] = await getAllPlayers();
+
+//     const newPlayer: Player = player;
+//     newPlayer.statistics.overall = await estimateOverallStat(player.statistics);
+
+//     console.log(newPlayer);
+// }
+
+// insertPlayer({name: "d", nationality: "d", position: "Forward", statistics: {defending:1,dribbling:1,pace:1,passing:1,physical:1,shooting:3}});
