6e9d80fef3a87b64a69eb037e27f15509f59fcd8
JulioCesar-A <juliocesar02e@gmail.com>
2025-08-31 19:47:00 -0300

    Club Services: Adicionar serviços de busca getAllClubs, getClubById, e getClubByName

--- src/services/club-services.ts
+++ src/services/club-services.ts
@@ -61,3 +61,93 @@ export const insertClubService = async(club: Club) : Promise<ClubTransferModel> 
     }
     return clubResponse;
 }
+
+export const getAllClubsService = async() : Promise<ClubTransferModel> => {
+    let clubResponse: ClubTransferModel = {
+        statusCode: 0,
+        body: []
+    }
+
+    let clubs: ClubResponse[];
+
+    try {
+        clubs = await getAllClubs();
+    } catch (error) {
+        const errorMsg = error ? String(error) : '';
+        clubResponse = {
+            statusCode: StatusCode.InternalServerError,
+            body: errorMsg
+        }
+        return clubResponse;
+    }
+
+    clubResponse = {
+        statusCode: StatusCode.OK,
+        body: clubs
+    }
+
+    return clubResponse;
+}
+
+export const getClubByIdService = async(id: number) : Promise<ClubTransferModel> => {
+    let clubResponse: ClubTransferModel = {
+        statusCode: 0,
+        body: []
+    }
+
+    let club: ClubResponse;
+
+    try {
+        club = await getClubById(id);
+    } catch (error) {
+        const errorMsg = error ? String(error) : '';
+
+        if (errorMsg.includes('not found')){
+        clubResponse = {
+            statusCode: StatusCode.NotFound,
+            body: errorMsg
+        }
+        } else {
+            clubResponse = {
+                statusCode: StatusCode.InternalServerError,
+                body: errorMsg
+            }
+        }
+
+        return clubResponse;
+    }
+
+    clubResponse = {
+        statusCode: StatusCode.OK,
+        body: club
+    }
+
+    return clubResponse;
+}
+
+export const getClubByNameService = async(name: string) : Promise<ClubTransferModel> => {
+    let clubResponse: ClubTransferModel = {
+        statusCode: 0,
+        body: []
+    }
+
+    let club: ClubResponse;
+
+    try {
+        club = await getClubByName(name);
+    } catch (error) {
+        const errorMsg = error ? String(error) : '';
+        clubResponse = {
+            statusCode: StatusCode.InternalServerError,
+            body: errorMsg
+        }
+        return clubResponse;
+    }
+
+    clubResponse = {
+        statusCode: StatusCode.OK,
+        body: club
+    }
+
+    return clubResponse;
+}
